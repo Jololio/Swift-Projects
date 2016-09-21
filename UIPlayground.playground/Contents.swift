bf5121d57ef757d8b8de77310a3afe88fbf0ddaa
Jololio <jala9132@740s-pl-120.local>
2016-09-21 11:41:10 -0600

    Added UI Project

---
+++ UIPlayground.playground/Contents.swift
@@ -0,0 +1,12 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+var DynamicView=UIview( frame:
+    CGRectMake(100, 200, 100, 100))
+DynamicView.backgroundColor=UIColor.greenColor()
+DynamicView.layer.cornerRadius=25
+DynamicView.layer.borderWidth=2
+DynamicView
+
+
+
