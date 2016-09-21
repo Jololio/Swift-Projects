7102af382b0ad1f028a9812c159b445f81c3702a
Jololio <jala9132@740s-pl-120.local>
2016-09-21 12:02:52 -0600

    Fixed UI Code

--- UIPlayground.playground/Contents.swift
+++ UIPlayground.playground/Contents.swift
@@ -1,8 +1,7 @@
 //: Playground - noun: a place where people can play
 
 import UIKit
-var DynamicView=UIview( frame:
-    CGRectMake(100, 200, 100, 100))
+var DynamicView = UIView(frame: CGRectMake(100, 200, 100, 100))
 DynamicView.backgroundColor=UIColor.greenColor()
 DynamicView.layer.cornerRadius=25
 DynamicView.layer.borderWidth=2
